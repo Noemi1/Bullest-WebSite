67f4edb92574323f5adf7e0f29893f7b288dd6ef
Noemi1 <noemi.almeida@aluno.faculdadeimpacta.com.br>
2020-09-11 09:39:53 -0300

    Ajustes de depoimentos

--- src/app/shared/depoimentos.model.ts
+++ src/app/shared/depoimentos.model.ts
@@ -1,9 +1,8 @@
 export class DepoimentosModel {
-    Empresa: string;
-    Imagem: string;
-    Cargo: string;
-    Pessoa: string;
-    Data: string;
-    Depoimento: string;
-  }
-  
+  Empresa: string;
+  Imagem?: string;
+  Cargo: string;
+  Pessoa: string;
+  Data: string;
+  Depoimento: string;
+}
--- src/app/shared/depoimentos.ts
+++ src/app/shared/depoimentos.ts
@@ -2,7 +2,7 @@ import { DepoimentosModel } from "./depoimentos.model";
 export const depoimentos: DepoimentosModel[] = [
     {
         Empresa: `Banco Santander`,
-        Imagem: `../../assets/img/depoimentos/santander.png`,
+        // Imagkem: `../../assets/img/depoimentos/santander.png`,
         Cargo: `Ger. Venda de Serviços PJ`,
         Pessoa: `Hendrix Azevedo de Carvalho`,
         Data: `18 de agosto de 2017`,
@@ -17,21 +17,20 @@ export const depoimentos: DepoimentosModel[] = [
         Cargo: `Gestor Comercial`,
         Pessoa: `Carlos `,
         Data: ``,
-        Depoimento:  `Ficou sensacional! Muito melhor que todos os produtos de mercado!
+        Depoimento: `Ficou sensacional! Muito melhor que todos os produtos de mercado!
         Ele atende exatamente a nossa realidade, é muito fácil e personalizado!`,
     }, {
-        Empresa: `Santander S3
-        `,
-        Imagem: `../../assets/img/depoimentos/santanderS3.png`,
+        Empresa: `Santander S3`,
+        // Imagem: `../../assets/img/depoimentos/santanderS3.png`,
         Cargo: `Reguladores e Liquidação SAM`,
         Pessoa: `Juliana Visse Munhoz`,
         Data: `11 de março de 2020`,
-        Depoimento:  `Agradeço sua equipe pela ajuda que deram no projeto[...]
+        Depoimento: `Agradeço sua equipe pela ajuda que deram no projeto[...]
         Com a ajuda de vocês, estamos reduzindo o trabalho manual.
         Parabéns a equipe!`,
     }, {
         Empresa: `Santander S3`,
-        Imagem: `../../assets/img/depoimentos/santanderS3.png`,
+        // Imagem: `../../assets/img/depoimentos/santanderS3.png`,
         Cargo: `Diretoria de Riscos & Compilance`,
         Pessoa: `Fábio José Miranda`,
         Data: `26 de março de 2020`,
